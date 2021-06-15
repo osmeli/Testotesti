1aaa00c907b51810d932638c13e0eb8204d4c870
osmeli <osmeli15@hotmail.com>
2021-06-15 14:49:35 +0300

    muut oksia

--- App.js
+++ App.js
@@ -3,11 +3,18 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, TextInput, Button} from 'react-native';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
+      <View>
       <Image resizeMode={"contain"} style={styles.image} source={require("./assets/logo.png")} />
-      <Text>What clubs do I Suck With?</Text>
-      <Image resizeMode={"contain"} style={styles.image_mailat} source={require("./assets/mailat.jpg")} />
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.navitext}><Button title='ADD SHOT' onPress={}></Button></Text>
+        <Text style={styles.navitext}><Button title='MY CLUBS' onPress={}></Button></Text>
+        <Text style={styles.navitext}><Button title='STATS' onPress={}></Button></Text>
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -18,6 +25,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center'
+  },
+  body: {
+    flex: 1,
+    backgroundColor: 'orange',
+    marginTop: -33,
+    textAlign: 'center'
+  },
+  navitext: {
+    fontWeight: 'bold',
+    paddingVertical: 50,
+    paddingHorizontal: 130,
+    fontSize: 30,
+    color: 'white'
   },
   image: {
     maxWidth: 400
